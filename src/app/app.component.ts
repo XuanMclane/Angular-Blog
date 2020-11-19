ad840b6311a0eb0620c1e4ed627d6c6f80987dfa
XuanMclane <xuanmclane@gmail.com>
2020-11-19 12:36:02 -0500

    pass data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,5 @@
 import { Component } from '@angular/core';
 
-interface Alert {
-  type: string;
-  message: string;
-}
-
-const ALERTS: Alert[] = [{
-    type: 'success',
-    message: 'This is an success alert',
-  }, {
-    type: 'info',
-    message: 'This is an info alert',
-  }, {
-    type: 'warning',
-    message: 'This is a warning alert',
-  }, {
-    type: 'danger',
-    message: 'This is a danger alert',
-  }, {
-    type: 'primary',
-    message: 'This is a primary alert',
-  }, {
-    type: 'secondary',
-    message: 'This is a secondary alert',
-  }, {
-    type: 'light',
-    message: 'This is a light alert',
-  }, {
-    type: 'dark',
-    message: 'This is a dark alert',
-  }
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -39,17 +7,9 @@ const ALERTS: Alert[] = [{
 })
 export class AppComponent {
   title = 'blog';
-  alerts: Alert[] = [];
-
-  constructor() {
-    this.reset();
-  }
-
-  close(alert: Alert) {
-    this.alerts.splice(this.alerts.indexOf(alert), 1);
-  }
-
-  reset() {
-    this.alerts = Array.from(ALERTS);
+  data= {
+    name: "Bruce",
+    age: 20,
+    email: "bruceli@gmail.com"
   }
 }
