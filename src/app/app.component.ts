690a54dca4b65f3fccda52463d5e407b4da28168
XuanMclane <xuanmclane@gmail.com>
2020-11-18 20:07:36 -0500

    Get Text Value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
-  myEvent(evt)
+  currentVal = ""
+  getVal(val)
   {
-    alert(evt)
+    console.warn(val)
+    this.currentVal = val
   }
 }
