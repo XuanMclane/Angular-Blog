10e8da6d9ee1fdd12dfdf59b4d7fe18c1aa3121b
XuanMclane <xuanmclane@gmail.com>
2020-11-18 20:48:05 -0500

    If else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
-  name = "Bruce"
-  disabledBox=true
-  enableBox()
-  {
-    this.disabledBox = false
-  }
+  show = "black"
 }
